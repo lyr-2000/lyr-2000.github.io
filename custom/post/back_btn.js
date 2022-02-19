551d47b03f5fcfb64ccc77374a0567991f3e2c3f
lyr-2000 <lyr-2000@users.noreply.github.com>
2022-02-19 07:08:07 +0000

    deploy: lyr-2000/lyr-2000.github.io@ccb3410cff38d6307e14048619351ab345990325

--- custom/post/back_btn.js
+++ custom/post/back_btn.js
@@ -1,30 +1,30 @@
-;(function(){
+; (function () {
 
   // 添加 button 
   // console.log('add')
-  document.addEventListener('DOMContentLoaded', function(event) {
+  window.addEventListener('load', function (event) {
     console.log("DOM fully loaded and parsed");  // 先打印
-    if(!$) {
+    if (!$) {
       return;
     }
     var back = $('.button.back')
-    back.on('click',function() {
-      if(document.referrer) {
+    back.on('click', function () {
+      if (document.referrer) {
         window.history.back(-1);
       }
     })
     var go = $('.button.go')
-    go.on('click' ,function(){
+    go.on('click', function () {
       window.history.forward();
     });
 
-    document.addEventListener('keydown',function(e) {
-      if(e.shiftKey) {
-        if(e.key =='ArrowLeft') {
+    document.addEventListener('keydown', function (e) {
+      if (e.shiftKey) {
+        if (e.key == 'ArrowLeft') {
           //left key
           back.trigger('click')
 
-        }else if (e.key=='ArrowRight' ) {
+        } else if (e.key == 'ArrowRight') {
           //right key 
           go.trigger('click')
         }
@@ -33,3 +33,33 @@
     })
   });
 })();
+;/* lazyload */
+
+; (function () {
+
+  window.addEventListener('load', function () {
+    var $ = window.$;
+    $.fn.extend({
+      LazyLoad: function () {
+        const $this = $(this);
+        if ($this.lazyload) {
+          $this.lazyload({
+            effect: 'fadeIn',
+            // placeholder_data_img: "https://cdn.jsdelivr.net/gh/moezx/cdn@3.0.2/img/svg/loader/trans.ajax-spinner-preloader.svg",
+          });
+        } else {
+          console.warn("没有图片懒加载插件");
+        }
+      }
+    });
+    console.log('use lazyload plugin => img.lazyload')
+    $('img.lazyload[data-original]').LazyLoad()
+    
+
+  });
+})();
+
+
+
+
+
